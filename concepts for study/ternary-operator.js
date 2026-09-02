e277b361047a74e45487f1250eb480c96760d61c
Aaron-Gregoire <aaronjemmett@gmail.com>
2026-09-02 18:13:10 -0400

    .checked property and setup for ternary operator

---
+++ concepts for study/ternary-operator.js
@@ -0,0 +1,5 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                    helps to assign a variable based on a condition
+//                    condition ? codeIfTrue : codeIfFalse;
+
+
